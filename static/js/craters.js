1625ad3391b56a3f511624fbd4ed209d90b7f3c1
arijguest <117730068+arijguest@users.noreply.github.com>
2024-11-24 19:54:56 +0000

    Update craters.js

--- static/js/craters.js
+++ static/js/craters.js
@@ -19,12 +19,11 @@ export class CraterManager {
 
     async loadData() {
         try {
-            const response = await fetch('earth-impact-craters-v2.geojson');
+            const response = await fetch('/static/data/earth-impact-craters-v2.geojson');
             if (!response.ok) throw new Error('Network response was not ok');
             
             const data = await response.json();
             this.allCraters = data.features;
-            this.processAges();
             return this.allCraters;
         } catch (error) {
             console.error('Error loading crater data:', error);
