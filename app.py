168b87b3d3f50484ea9c0df228e57fb5310af33a
arijguest <117730068+arijguest@users.noreply.github.com>
2024-11-24 17:11:14 +0000

    Update app.py

--- app.py
+++ app.py
@@ -40,9 +40,5 @@ def server_error(e):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(
-        host='0.0.0.0',
-        port=port,
-        debug=os.getenv('FLASK_ENV') == 'development'
-    )
+    app.run(host='0.0.0.0', port=port)
 
